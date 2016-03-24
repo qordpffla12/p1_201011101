e678ae4d8cae0ebf0b31d1d1eeeeed9a24ce811c
qordpffla12 <qordpffla12@naver.com>
2016-03-24 17:08:27 +0900

    4주차 과제
    사용자 입력 받아 도형 그리기
    &
    사용자 입력 받아 섭씨 화씨 전환하기

---
+++ w4Main.py
@@ -0,0 +1,19 @@
+﻿sel = raw_input("T or S: ")
+
+if(sel=='T'):
+    t1.fd(100)
+    t1.left(120)
+    t1.fd(100)
+    t1.left(120)
+    t1.fd(100)
+    t1.left(120)
+    
+else:
+    t1.fd(100)
+    t1.left(90)
+    t1.fd(100)
+    t1.left(90)
+    t1.fd(100)
+    t1.left(90)
+    t1.fd(100)
+    
---
+++ w4Temp.py
@@ -0,0 +1,11 @@
+﻿fc = raw_input("F or C?: ")
+temp = raw_input("Insert temperature: ")
+
+if(fc =='F'):
+    print ((float(temp)-32)/1.8)
+    
+elif(fc=='C'):
+    print (float(temp)*1.8+32)
+    
+else:
+    print('You entered wrong temperature')
