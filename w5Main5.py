17d2f4d555250aa57f9143ad764496c2c79f93c4
qordpffla12 <qordpffla12@naver.com>
2016-03-30 15:26:32 +0900

    compute grade

---
+++ w5Main5.py
@@ -0,0 +1,17 @@
+﻿markTemp = raw_input("insert marks (0~100): ")
+marks = float(markTemp)
+
+def computeGrade(marks):
+    if(90<=marks and marks<=100):
+        grade = 'A'
+    elif(80<=marks and marks<90):
+        grade = 'B'
+    elif(70<=marks and marks<80):
+        grade = 'C'
+    elif(60<=marks and marks<70):
+        grade = 'D'
+    else:
+        grade = 'F'
+    return grade
+
+computeGrade(marks)
