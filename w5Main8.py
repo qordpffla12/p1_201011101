4666aec54b5d3239bc8a461885f4be9224d94d58
qordpffla12 <qordpffla12@naver.com>
2016-04-04 23:42:42 +0900

    BMI 다이어그램 & 소스코드

---
+++ w5Main8.py
@@ -0,0 +1,22 @@
+﻿def getBMI(hei,wei):
+    hei = float(raw_input("Insert your height: "))
+    wei = float(raw_input("Insert your weight: "))
+    bmi = wei/(hei/100*hei/100)
+    
+    if bmi<18.5:
+        res = 'under average'
+    elif bmi>=18.5 and bmi <24.9:
+        res = 'average'
+    elif bmi>=24.9 and bmi <29.9:
+        res = 'over average'
+    else:
+        res = 'biman'
+    return res, round(bmi, 2)
+
+
+def main():
+	getBMI(hei,wei)
+
+
+if __name__=="__main__":
+    main()
