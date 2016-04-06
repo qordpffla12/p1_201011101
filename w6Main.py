d5cb896d0f34c2c53f1e73f94473a782070922d0
qordpffla12 <qordpffla12@naver.com>
2016-04-06 15:02:43 +0900

    3or5 배수 다이어그램&코드

---
+++ w6Main.py
@@ -0,0 +1,15 @@
+﻿def getMultiplesOf3_5(begin,end):
+   sum = 0
+   for i in range(begin, end):
+       if i%3 ==0 or i%5 == 0:
+       sum = sum+i
+   return sum
+
+def lab6():
+	getMultiplesOf3_5(1,1000)
+
+def main():
+	lab6()
+
+if __name__=="__main__":
+    main()
