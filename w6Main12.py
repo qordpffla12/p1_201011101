65fe3f91c3c7cda6517cd0fe1890275ebb4458db
qordpffla12 <qordpffla12@naver.com>
2016-04-06 23:34:10 +0900

    updown 게임

---
+++ w6Main12.py
@@ -0,0 +1,19 @@
+﻿import random
+
+def upDown(begin, end):
+    rn = random.randrange(begin, end, 1)
+    num = 0
+    count = 0
+
+    while num != rn:
+        num = int(raw_input("enter num: "))
+        count = count+1
+
+        if num < rn:
+            print 'up'
+        elif num > rn:
+            print 'down'
+        else:
+            print 'right', count
+
+upDown(1,100)
