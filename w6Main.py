3aa0fd64a913d19eab6e1f7c604362fc28671e78
qordpffla12 <qordpffla12@naver.com>
2016-04-06 15:31:14 +0900

    3_5배수합 & 윤년

--- w6Main.py
+++ w6Main.py
@@ -5,9 +5,17 @@
        sum = sum+i
    return sum
 
+def getYoonnyeon(year):
+    year =int(raw_input("insert year: "))
+    if (year%4 == 0) and (year%100 !=0 or year%400==0):
+        res = 'Yoonnyeon'
+    else:
+        res = 'not Yoonnyeon'
+    return res
+
 def lab6():
 	getMultiplesOf3_5(1,1000)
-
+	getYoonnyeon(2011)
 def main():
 	lab6()
 
