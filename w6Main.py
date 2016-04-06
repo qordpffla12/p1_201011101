f5a9eb4fd919891ca756339f2d170011fa18ab3a
qordpffla12 <qordpffla12@naver.com>
2016-04-06 23:29:17 +0900

    3_5배수합 & 윤년 수정

--- w6Main.py
+++ w6Main.py
@@ -1,12 +1,12 @@
-﻿def getMultiplesOf3_5(begin,end):
-   sum = 0
-   for i in range(begin, end):
-       if i%3 ==0 or i%5 == 0:
-       sum = sum+i
-   return sum
+﻿def getMultiplesOf3_5(begin, end):
+    sum = 0
+    for i in range(begin, end):
+        if i%3 ==0 or i%5 == 0:
+            sum = sum+i
+    return sum
 
 def getYoonnyeon(year):
-    year =int(raw_input("insert year: "))
+    
     if (year%4 == 0) and (year%100 !=0 or year%400==0):
         res = 'Yoonnyeon'
     else:
@@ -15,7 +15,7 @@ def getYoonnyeon(year):
 
 def lab6():
 	getMultiplesOf3_5(1,1000)
-	getYoonnyeon(2011)
+	getYoonnyeon(2012)
 def main():
 	lab6()
 
