2aa131009bbda8b90878fef2a87f34465ebcbd69
qordpffla12 <qordpffla12@naver.com>
2016-04-11 14:29:50 +0900

    sumlist

---
+++ sumList.py
@@ -0,0 +1,14 @@
+﻿   
+def sumList(list):
+    sum=0
+    for i in range(0, len(list)):
+        sum+=list[i]
+    return sum
+
+def lab7():
+    x=[1,2,3]
+    mysum=sumList(x)
+    print mysum
+
+def main()
+    lab7()
