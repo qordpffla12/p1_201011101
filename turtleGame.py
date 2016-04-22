d5e6c04cb492936301be06317794eadde09fa787
qordpffla12 <qordpffla12@naver.com>
2016-04-22 23:12:36 +0900

    거북이게임
    거북이게임 v0.5 입니다.
    장애물이 위에서 내려오고 플레이어가 그것을 피하는 방식으로 고전에 유행하던 일명 똥피하기 게임 형식으로 구현해보고자 했습니다.
    while문을 돌릴때 플레이어 정보만 입력하면 잘 돌아가는데 장애물(obstacles)들을 넣으면 움직임과 속도가 상당히
    부자연스러워졌습니다...
    많은 점을 보완해서 v1.0에서는 완벽하게 구현,제출하겠습니다.

---
+++ turtleGame.py
@@ -0,0 +1,71 @@
+﻿import turtle
+import random
+
+#게임공간 세팅
+wn=turtle.Screen()
+wn.bgcolor("pink")
+stadium=turtle.Turtle()
+stadium.speed(10)
+stadium.penup()
+stadium.setpos(-300,-300)
+stadium.pendown()
+stadium.pensize(2)
+for side in range(4):
+    stadium.forward(600)
+    stadium.left(90)
+stadium.hideturtle()
+
+#사용자 세팅
+player=turtle.Turtle()
+player.penup()
+player.setpos(0,-290)
+player.shape("circle")
+
+#장애물 세팅
+maxObstacles = random.randint(10,20)
+obstacles =list()
+
+#획득할 먹이의 모양과 나타날 위치를 랜덤으로 지정
+for i in range(maxObstacles):
+    obstacles.append(turtle.Turtle())
+    obstacles[i].color("blue")
+    obstacles[i].shape("circle")
+    obstacles[i].penup()
+    obstacles[i].speed(0)
+    obstacles[i].setposition(random.randint(-300,300), random.randint(300,300))
+
+speed=30
+
+def pressLeft():
+    player.setheading(180)
+def pressRight():
+    player.setheading(360)
+def upSpeed():
+    global speed
+    speed+=10
+def downSpeed():
+    global speed
+    speed-=10
+
+#키보드 입력 세팅
+turtle.listen()
+turtle.onkey(pressLeft,"Left")
+turtle.onkey(pressRight,"Right")
+turtle.onkey(upSpeed, "Up")
+turtle.onkey(downSpeed, "Down")
+    
+
+
+while obstacles[i].ycor() > -280:
+    player.forward(speed)
+    player.speed(3)
+    
+    #장애물이 하늘에서 내려오기
+    for i in range(maxObstacles):
+        obstacles[i].setheading(270)
+        obstacles[i].forward(speed)
+        obstacles[i].speed(3)
+        
+    #player가 벽에 부딪히면 뒤를 돌아보게 설정
+    if player.xcor() > 290 or player.xcor() < -290:
+        player.right(180)
