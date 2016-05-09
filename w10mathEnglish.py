3210bdccfdb723504454d9e69ae8e0a215a8e295
qordpffla12 <qordpffla12@naver.com>
2016-05-09 15:55:14 +0900

    10주 렛잇비

---
+++ w10mathEnglish.py
@@ -0,0 +1,13 @@
+﻿allData=[["English","Math"], [100,200], [200,200], [100,300]]
+data=allData[1:]
+engSum=0
+engAverage=0
+mathSum=0
+mathAverage=0
+for i in range(0,len(data)):
+    engSum += data[i][0]
+    mathSum += data[i][1]
+engAverage= engSum/3
+mathAverage= mathSum/3
+print "Sum of English:",engSum,"Average of English:",engAverage
+print "Sum of math:",mathSum,"Average of math:",mathAverage
---
+++ w10milk.py
@@ -0,0 +1,20 @@
+﻿allData=[
+    ["Coffee","Water","Milk","Icecream"],
+    ["Espresso","No","No","No"],
+    ["Long Black","Yes","No","No"],
+    ["Flat White","No","Yes","No"],
+    ["Cappuccino","No","Yes","No"],
+    ["Affogato","No","No","Yes"]
+]
+
+data=allData[1:]
+data
+
+milkCount=0.0
+milkB=0.0
+
+for i in data:
+    if i[2].upper()=='YES':
+        milkCount += 1
+milkB =milkCount/len(data)
+milkB
