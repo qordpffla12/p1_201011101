93ced231643046e3b58dc768a0d5cfd1aeb0a4bb
qordpffla12 <qordpffla12@naver.com>
2016-05-31 23:32:02 +0900

    12주차 과제

---
+++ w12plusfile.py
@@ -0,0 +1,9 @@
+﻿try:
+    fin1=open('python.txt','a')
+    fin2=open('outputNumbe.txt','r')
+    for line in fin2:
+        fin1.write(line)
+    fin1.close()
+    fin2.close()
+except Exception as e:
+    print e
---
+++ w12tap.py
@@ -0,0 +1,10 @@
+﻿data=[1,2,3,4,5,6,7,8,9,10]
+fout=open('outputNumber.txt', 'w')
+for i in data:
+    if i%2==0:
+        content='\n'
+    else:
+        content='\t'
+    toPrint="{0}{1}".format(i,content)
+    fout.write(toPrint)
+fout.close()
