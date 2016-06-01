be7eb146d6cc587490680a7fe982081eb7a124cc
qordpffla12 <qordpffla12@naver.com>
2016-06-01 15:38:00 +0900

    강아지 시츄 마르티스

---
+++ w13oohomework.py
@@ -0,0 +1,16 @@
+﻿class Dog(object):
+    def bark(self):
+        print "mung mung"
+class Sichew(Dog):
+    def bark(self):
+        print "si si"
+class Martis(Dog):
+    def bark(self):
+        print "mal mal"
+
+dogOne = Dog()
+dogTwo = Sichew()
+dogThree = Martis()
+dogOne.bark()
+dogTwo.bark()
+dogThree.bark()
